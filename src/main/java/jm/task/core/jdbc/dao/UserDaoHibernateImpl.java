492b457d4b54e0e94074e1b35a1ee7e3de344b39
aleexqq <shalygin.alexey1@gmail.com>
2024-07-19 00:11:26 +0700

    добавил hibernate

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,127 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
-    public UserDaoHibernateImpl() {
 
+    private static final SessionFactory sessionFactory = Util.getSessionFactory();
+
+    public UserDaoHibernateImpl() {
     }
 
 
     @Override
     public void createUsersTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS user_schema.users (" +
+                "id bigint NOT NULL AUTO_INCREMENT," +
+                "name varchar(50)," +
+                "lastName varchar(50)," +
+                "age TINYINT," +
+                "PRIMARY KEY (id))";
 
+        Transaction transaction = null;
+
+        try (Session sess = sessionFactory.openSession()) {
+            transaction = sess.beginTransaction();
+            sess.createSQLQuery(sql).executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
     }
 
     @Override
     public void dropUsersTable() {
+        String sql = "DROP TABLE IF EXISTS user_schema.users";
 
+        Transaction transaction = null;
+
+        try (Session sess = sessionFactory.openSession()) {
+            transaction = sess.beginTransaction();
+            sess.createSQLQuery(sql).executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        String sql = "INSERT INTO user_schema.users (name, lastName, age) VALUES (?, ?, ?)";
 
+        Transaction transaction = null;
+
+        try (Session sess = sessionFactory.openSession()) {
+            transaction = sess.beginTransaction();
+            sess.createSQLQuery(sql)
+                    .setParameter(1, name)
+                    .setParameter(2, lastName)
+                    .setParameter(3, age)
+                    .executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
     }
 
     @Override
     public void removeUserById(long id) {
+        String sql = "DELETE FROM user_schema.users WHERE id = ?";
 
+        Transaction transaction = null;
+
+        try (Session sess = sessionFactory.openSession()) {
+            transaction = sess.beginTransaction();
+            sess.createSQLQuery(sql)
+                    .setParameter(1, id)
+                    .executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        String sql = "SELECT * FROM user_schema.users";
+
+        List<User> list;
+        try (Session sess = sessionFactory.openSession()) {
+            list = (List<User>) sess.createSQLQuery(sql)
+                    .addEntity(User.class)
+                    .list();
+        }
+        return list;
     }
 
     @Override
     public void cleanUsersTable() {
+        String sql = "TRUNCATE TABLE user_schema.users";
 
+        Transaction transaction = null;
+
+        try (Session sess = sessionFactory.openSession()) {
+            transaction = sess.beginTransaction();
+            sess.createSQLQuery(sql).executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,8 +1,13 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
 
@@ -10,7 +15,36 @@ public class Util {
     private final static String USER = "root";
     private final static String PASSWORD = "ud5y3y";
 
+    private static SessionFactory sessionFactory;
+
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration config = new Configuration();
+
+                Properties props = new Properties();
+                props.put(Environment.URL, URL);
+                props.put(Environment.USER, USER);
+                props.put(Environment.PASS, PASSWORD);
+
+                config.setProperties(props);
+                config.addAnnotatedClass(User.class);
+
+                sessionFactory = config.buildSessionFactory();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        if (sessionFactory != null) {
+            sessionFactory.close();
+        }
     }
 }
